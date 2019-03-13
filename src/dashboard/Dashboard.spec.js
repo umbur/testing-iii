bf9e2c009ab5f6c2dc4ec7ed2cb82f52675e0c65
umbur <you@example.com>
2019-03-13 15:58:28 -0600

    created tests for Dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
 // Test away
+ import React from 'react';
+ import { render } from 'react-testing-library';
+ import Dashboard from './Dashboard.js';
+
+ describe('<Describe />', () => {
+     it('should render without crashing', () => {
+         render(<Dashboard/>)
+     })
+ })
