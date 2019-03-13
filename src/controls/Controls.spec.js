9a3e9af1fb17a1b931a5942af7e01094337ea909
umbur <you@example.com>
2019-03-13 15:50:58 -0600

    created tests for Controls component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,35 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import Controls from './Controls';
+
+const mock = jest.fn()
+
+describe('<Controls />', () => {
+    it('should call the toggleLocked function passed as prop to unlock the gate', () => {
+        const { getByText } = render(<Controls locked = {true} closed={true} toggleLocked={mock} />)
+        fireEvent.click(getByText(/unlock/i))
+        expect(mock).toHaveBeenCalledTimes(1)
+    })
+    it('should call the toggleClosed function passed as prop to open an unlocked closed gate', () => {
+        const { getByText } = render(<Controls locked={false} closed={true} toggleLocked={mock} toggleClosed={mock} />);
+        fireEvent.click(getByText(/open/i));
+        expect(mock).toHaveBeenCalledTimes(1);
+        mock.mockClear();
+    })
+    it('should call the toggleClosed function passed as prop to close an open gate', () => {
+        const { getByText } = render(<Controls locked={false} closed={false} toggleLocked={mock} toggleClosed={mock} />);
+        fireEvent.click(getByText(/close/i));
+        expect(mock).toHaveBeenCalledTimes(1);
+        mock.mockClear();
+    })
+    it('should call the toggleLocked function passed as prop to lock an unlocked closed gate', () => {
+        const { getByText } = render(<Controls locked={false} closed={true} toggleLocked={mock} toggleClosed={mock} />);
+        fireEvent.click(getByText(/lock/i));
+        expect(mock).toHaveBeenCalledTimes(1);
+        mock.mockClear();
+    });
+
+    
+
+})
